bcfaaab6009347a7c1347306495f2cbf0805800b
awais7796 <awaitzz49@gmail.com>
2025-02-20 19:50:28 +0530

    read , write , append via fs

--- week-2/01-async-js/easy/4-write-to-file.js
+++ week-2/01-async-js/easy/4-write-to-file.js
@@ -4,15 +4,24 @@ Using the fs library again, try to write to the contents of a file.
 You can use the fs library to as a black box, the goal is to understand async tasks.
 */
 const fs = require ('fs');
-
-
 console.log("befor fs");
+
+// writeFile overwrite the content of the file 
+fs.writeFile('example.txt','this is written via fs',(err)=>{
+    if(err){
+        console.log("the error is ",err);
+        
+    }
+    console.log("wrote via writeFile");
+    
+})
+// appendFile does not overwrite the file 
 fs.appendFile('example.txt','this is writtem via fs',(err)=>{
     if(err){
         console.log("the error is ",err);
         
     }
-    console.log("wrote");
+    console.log("wrote via appnendFile");
     
 })
 fs.readFile('example.txt','utf8',(err,data)=>{
@@ -20,11 +29,23 @@ fs.readFile('example.txt','utf8',(err,data)=>{
         console.log("error in read fs");
         
     }
-    console.log(data);
+    console.log("readFile : ",data);
 })
 
+// expensive operation for async 
 let sum=0;
 for(let i=0;i<=100000;i++){
     sum+=i;
 }
 console.log(sum);
+
+
+
+/** 
+                                output
+ befor fs
+5000050000
+wrote via writeFile
+wrote via appnendFile
+this is written via fs
+ * */ 
